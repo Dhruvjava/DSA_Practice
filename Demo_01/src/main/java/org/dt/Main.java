2ce125b5aa83a4d307a72501e6f4da37467e712b
Dhruv Kumar <61408179+Dhruvjava@users.noreply.github.com>
2022-11-19 16:35:41 +0530

    Initial commit

---
+++ Demo_01/src/main/java/org/dt/Main.java
@@ -0,0 +1,27 @@
+package org.dt;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int low=sc.nextInt();
+    int hight=sc.nextInt();
+
+    for(int n=low;n<= hight; n++){
+      boolean isPrime=true;
+      int div=2;
+      while(div*div <= n){
+        if(n%div == 0){
+          isPrime=false;
+        }
+        div++;
+      }
+      if(isPrime){
+        System.out.println(n);
+      }
+    }
+  }
+}
