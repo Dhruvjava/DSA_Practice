a5c34cef831a3e510affd5c152326e16491f199c
Dhruvjava <61408179+Dhruvjava@users.noreply.github.com>
2022-12-07 10:51:07 +0530

    new code i for sum two element from array that are equal to targetSum.

---
+++ Demo_01/src/main/java/org/dt/AddTwoNumEqualTarget.java
@@ -0,0 +1,37 @@
+package org.dt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AddTwoNumEqualTarget {
+
+  public static void main(String[] args) throws IOException {
+    InputStreamReader isr = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(isr);
+    int length = Integer.parseInt(reader.readLine());
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++) {
+      array[i] = Integer.parseInt(reader.readLine());
+    }
+    int targetSum = Integer.parseInt(reader.readLine());
+    int[] result=targetSum(array, targetSum);
+    for (int num : result){
+      System.out.print(num+" ");
+    }
+  }
+
+  private static int[] targetSum(int[] array, int targetSum) {
+    Set<Integer> set=new HashSet<>();
+    for (int num : array){
+      int sum = targetSum - num;
+      if(set.contains(sum)){
+        return new int[]{sum, num};
+      }
+      set.add(num);
+    }
+    return new int[0];
+  }
+}
