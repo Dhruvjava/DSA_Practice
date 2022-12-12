82169b445f1868ebc9381449f8223e8e62a29e9e
Dhruvjava <61408179+Dhruvjava@users.noreply.github.com>
2022-12-12 16:27:49 +0530

    Integer Rotational Optimize code in Java

--- Demo_01/src/main/java/org/dt/RotateInteger.java
+++ Demo_01/src/main/java/org/dt/RotateInteger.java
@@ -11,22 +11,17 @@ public class RotateInteger {
     BufferedReader reader = new BufferedReader(isr);
     int n = Integer.parseInt(reader.readLine());
     int k = Integer.parseInt(reader.readLine());
-    int divider = 1;
+    int divider = (int) Math.pow(10,k);
     int i = 0;
     int test = n;
-    int divtest = k;
-    while (divtest > 0) {
-      divider *= 10;
-      divtest--;
-    }
     while (test > 0) {
       test /= 10;
       i++;
     }
-    int q = n / divider;
-    int r = n % divider;
-    int ch = (int) (r * Math.pow(10, (i - k)));
-    System.out.println(ch + q);
+    int result = n / divider;
+    int remender = n % divider;
+    int rotationalVal = (int) (remender * Math.pow(10, (i - k)));
+    System.out.println(rotationalVal + result);
   }
 
 }
