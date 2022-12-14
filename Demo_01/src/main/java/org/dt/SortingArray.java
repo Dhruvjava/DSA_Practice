51b03136f5830dc24ef0f553e69dce9c6fdeaef2
Dhruvjava <61408179+Dhruvjava@users.noreply.github.com>
2022-12-14 10:19:32 +0530

    Sorting the array

---
+++ Demo_01/src/main/java/org/dt/SortingArray.java
@@ -0,0 +1,41 @@
+package org.dt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SortingArray {
+
+  public static void main(String[] args) throws IOException {
+    InputStreamReader isr = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(isr);
+    int len = Integer.parseInt(reader.readLine());
+    int[] array = new int[len];
+    for (int i=0; i<len; i++) {
+      array[i] = Integer.parseInt(reader.readLine());
+    }
+    array = sort(array,len);
+
+    for (int i=0; i<len; i++) {
+      System.out.print(array[i]+ " ");
+    }
+  }
+
+  public static int[] sort(int[] array, int len) {
+    int temp = 0;
+    int p = 0;
+    for (int i = 0; i < len; i++) {
+      temp = array[i];
+      for (int j = i+1; j < len; j++) {
+        if (temp > array[j]) {
+          temp = array[j];
+          p = j;
+        }
+      }
+      array[p] = array[i];
+      array[i] = temp;
+    }
+    return array;
+  }
+
+}
