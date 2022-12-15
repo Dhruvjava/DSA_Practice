714940761282d062b52f3c244cfaef0cf62a1365
Dhruvjava <61408179+Dhruvjava@users.noreply.github.com>
2022-12-15 16:03:40 +0530

    LCM and GCD Codes With optimization.

--- Demo_01/src/main/java/org/dt/LCM.java
+++ Demo_01/src/main/java/org/dt/LCM.java
@@ -4,27 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class GCDExample {
-
-  public static void main(String[] args) throws IOException {
+public class LCM {
+    public static void main(String[] args) throws IOException {
     InputStreamReader isr= new InputStreamReader(System.in);
     BufferedReader reader= new BufferedReader(isr);
     int x=Integer.parseInt(reader.readLine());
     int y=Integer.parseInt(reader.readLine());
-    int result;
-//    if (x > y) {
-//      result=getGcd(y,x);
-//    }
-//    else {
-//      result=getGcd(x,y);
-//    }
-//    System.out.println(result);
-
-    result = getGcd(x,y);
-    System.out.println("GCD of the given number is :"+result);
-
+    int result = getGcd(x,y);
+    int lcm = (x*y)/result;
+    System.out.println("LCM of the given number is :"+lcm);
   }
-  public static int getGcd(int x, int y) {
+    public static int getGcd(int x, int y) {
     int result = 0;
     while (y !=0){
       result=y;
